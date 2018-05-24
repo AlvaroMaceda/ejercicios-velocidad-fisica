a3702e23cc36834520f482e943ddcae9ddec5523
Alvaro Maceda <alvaro@alvaromaceda.es>
2018-05-24 12:59:49 +0200

    Funcionando con webpack sin babel ni server

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
+import Movil from './Movil';
+
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 ctx.fillStyle = "#FF0000";
 ctx.fillRect(0,0,150,75);
 
 
-class Patata {
-    constructor() {
-        console.log('Hola');
-    }
-    foo() { alert('patata') }
-}
-
-import Movil from './Movil';
 
 let p= new Movil();
 p.foo();
